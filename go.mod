ed0f8e63472777821e9c5c55473249dfc96d09f7
Oskar Havo <oskarhavo@hotmail.com>
2021-11-08 16:42:10 +0100

    Initial GO commit (skeleton from previous course)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module maain
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+// Entrypoint
+func main() {
+	addrs,err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+	var IP net.IP
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				IP = ipnet.IP
+				fmt.Println(ipnet.IP.String() + "\n")
+			}
+		}
+	}
+	network := NewNetwork(&IP, NewMessageService(false,nil))
+	fmt.Println("Network IP is  " + IP.String())
+
+	//Create Threads.
+	go network.HTTPlisten()
+
+}
