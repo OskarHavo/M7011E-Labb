4f8d4c0db86c9cd023d4e3e36009c7bbc530c090
robdan7 <robdan-7@student.ltu.se>
2021-11-15 16:59:35 +0100

    switched port

--- main.go
+++ main.go
@@ -3,20 +3,5 @@ package main
 
 // Entrypoint
 func main() {
-	// Create Network.
-	//network := NewNetwork()
-
-	//Create Threads.
-	//go network.listenToHTTP()
-
-	// Infinite Loop
-	//select {}
-
-	listenToHTTP()
-
-	db := startDatabase()
-	checkVersion(db)
-	readAllFromDatabase(db)
-	addToDatabase("Wind", "Boden", "2019", db)
-	readAllFromDatabase(db)
+	listenToHTTP(":2222")
 }
