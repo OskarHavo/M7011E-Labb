a59b6066187dbd856902551af6a80ae8d9f177d5
Oskar Havo <oskarhavo@hotmail.com>
2021-11-16 15:33:36 +0100

    New Calculation of consumption, added households, windmill outages

--- dataGeneration.go
+++ dataGeneration.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"math/rand"
 	"time"
 )
 
+// TODO error on high years  (4+)---> listOfDates[i] = date.Format("2006-01-02")
 // Create range of dates from the appointed start date until N years.
 func createDateRange(year int, month time.Month, day int, numberOfYears int) []string {
 	listOfDates := make([]string, getNumberOfDays(numberOfYears))
@@ -20,7 +20,6 @@ func createDateRange(year int, month time.Month, day int, numberOfYears int) []s
 		listOfDates[i] = date.Format("2006-01-02")
 		i = i+1
 	}
-	//fmt.Println(listOfDates)
 	return listOfDates
 }
 
@@ -33,6 +32,123 @@ func randomizeFloats(minimumFloatValue float64, maximumFloatValue float64, numbe
 		listOfRandomizedFloats[i] = minimumFloatValue + randomFloatValue * (maximumFloatValue- minimumFloatValue)
 	}
 	return listOfRandomizedFloats
+}
+
+// Creates and returns a list of N randomized float values within the range. This simulates Seasonal Changes.
+func randomizeSeasonalFloats(numberOfFloatsToRandomize int, numberOfYears int) []float64 {
+	listOfRandomizedFloats := make([]float64,numberOfFloatsToRandomize)
+
+	// Days in each month
+	january := 31
+	february := 28
+	march := 31
+	april := 30
+	may := 31
+	june := 30
+	july := 31
+	august := 31
+	september := 30
+	october := 31
+	november := 30
+	december := 31
+
+	dateCount := 0
+
+	for j := 0; j < numberOfYears; j++{
+
+		for i := dateCount ; i < january + dateCount; i++{
+			minimumFloatValue := 20.0
+			maximumFloatValue := 24.0
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + january
+
+		for i := dateCount ; i < february + dateCount; i++{
+			minimumFloatValue := 19.5
+			maximumFloatValue := 23.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + february
+
+		for i := dateCount ; i < march + dateCount; i++{
+			minimumFloatValue := 17.0
+			maximumFloatValue := 20.0
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + march
+
+		for i := dateCount ; i < april + dateCount; i++{
+			minimumFloatValue := 16.0
+			maximumFloatValue := 18.0
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + april
+
+		for i := dateCount ; i < may + dateCount; i++{
+			minimumFloatValue := 14.5
+			maximumFloatValue := 16.0
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + may
+
+		for i := dateCount ; i < june + dateCount; i++{
+			minimumFloatValue := 11.0
+			maximumFloatValue := 13.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + june
+
+		for i := dateCount ; i < july + dateCount; i++{
+			minimumFloatValue := 11.0
+			maximumFloatValue := 13.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + july
+
+		for i := dateCount ; i < august + dateCount; i++{
+			minimumFloatValue := 12.5
+			maximumFloatValue := 14.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + august
+
+		for i := dateCount ; i < september + dateCount; i++{
+			minimumFloatValue := 14.5
+			maximumFloatValue := 15.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + september
+
+		for i := dateCount ; i < october + dateCount; i++{
+			minimumFloatValue := 16.0
+			maximumFloatValue := 18.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + october
+
+		for i := dateCount ; i < november + dateCount; i++{
+			minimumFloatValue := 18.5
+			maximumFloatValue := 19.5
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + november
+
+		for i := dateCount ; i < december + dateCount; i++{
+			minimumFloatValue := 20.5
+			maximumFloatValue := 23.0
+			listOfRandomizedFloats[i] = minimumFloatValue + rand.Float64() * (maximumFloatValue- minimumFloatValue)
+		}
+		dateCount = dateCount + december
+
+		}
+		listOfRandomizedFloats[dateCount] = 17.1 // Last Element (Next January)
+	return listOfRandomizedFloats
+}
+
+// Get a random integer within the given range.
+func generateRandomInteger(minimumIntegerValue int,maximumIntegerValue int ) int{
+	randomInteger := minimumIntegerValue + rand.Intn(maximumIntegerValue-minimumIntegerValue+1)
+	return randomInteger
 }
 
 
@@ -56,72 +172,95 @@ func generateSineWaveFloats(minimumFloatValue float64, maximumFloatValue float64
 	return listOfRandomizedFloats
 }
 
+/*
 // Creates and returns a list of float values. This is based on a normal distribution inorder to replicate a day/night cycle in the floats produced.
-func generateNormalDistibutionFloats(amplitude float64, numberOfFloatsToRandomize int) []float64 {
-	listOfRandomizedFloats := make([]float64,numberOfFloatsToRandomize)
+func generateNormalDistibutionFloats(numberOfFloatsToRandomize int, numberOfYears int ) []float64 {
 
+
+	offset := calculateStepSizeOffset(numberOfFloatsToRandomize)
+	listOfRandomizedFloats := make([]float64,numberOfFloatsToRandomize+offset)
 	// Between 0.0-30 (amp) kWh
+	fmt.Println("OFFSET = ", offset)
 
-	amp := amplitude // Amplitude (Top Value)
-	mean := 0.2 // Mean (Middle Value)
-	std := 1.5 // Standard Deviation
+	amp := 30.0// Amplitude (Top Value)
+	mean := 0.0 // Mean (Middle Value)
+	std := 1.0 // Standard Deviation
 	variance := math.Pow(std,2)  // Variance
 
 	i := 0
 
-	startX, endX, stepSize := calculateNormalDistributionStep(numberOfFloatsToRandomize)
+	startX, endX, stepSize := calculateNormalDistributionStep(numberOfFloatsToRandomize+offset, numberOfYears)
+	fmt.Println("startX = ", startX, "endX = ", endX, "stepSize = ", stepSize)
+
+	// TODO Fixa stepSize så den funkar för alla år, hårdkoda? switch sats
 
 	// Calculates the points in the normal distribution
-	for x := startX ; x < endX ; x=x+stepSize{
+	for x := startX ; x < endX ; x=x+  stepSize{
+
 
 		listOfRandomizedFloats[i] = amp * math.Exp(-1.0 * math.Pow(x - mean,2.0) / (2.0*variance))
 		//fmt.Println(listOfRandomizedFloats[i])
 
 		i=i+1
+		if (i == numberOfFloatsToRandomize){
+			break
+		}
 	}
-	fmt.Println(i)
+	fmt.Println("NORMAL I = ", i)
 	return listOfRandomizedFloats
 }
+*/
 
+/*
 // Generate a Energy Price and Surplus Status for each date.
 func generateEnergyPrice(energyConsumption []float64, producedEnergy []float64) ([]float64, []bool){
-	numberOfFloatsToRandomize := len (energyConsumption)
+	numberOfFloatsToRandomize := len (producedEnergy)
 	listOfEnergyPrices := make([]float64,numberOfFloatsToRandomize)
 	listOfEnergySurplusStatus := make([]bool,numberOfFloatsToRandomize)
 
 	for i:= 0; i<numberOfFloatsToRandomize; i++{
 		dailyEnergyConsumption := energyConsumption[i] * 1.0
+
 		dailyEnergyProduced := producedEnergy[i] * 1.0
 
 		dailyElectrictyPrice, dailySurplusStatus := calculateDailyEnergyPrice(dailyEnergyConsumption,dailyEnergyProduced)
 
 		listOfEnergyPrices[i] = dailyElectrictyPrice
 		listOfEnergySurplusStatus[i] = dailySurplusStatus
-
-		//fmt.Println(listOfEnergyPrices[i],listOfEnergySurplusStatus[i])
 	}
 	return listOfEnergyPrices, listOfEnergySurplusStatus
 }
 
+/*
+// Check if the number of datapoints is odd or not. If so, make it even by adding an offset.
+func calculateStepSizeOffset(numberOfFloatsToRandomize int) int  {
+	if(numberOfFloatsToRandomize % 2 == 1){
+		return 1
+	}else{
+		return 0
+	}
+}
+
 // Calculates the range and step size inorder to get all values.
-func calculateNormalDistributionStep(numberOfFloatsToRandomize int) (float64,float64,float64){
-	startX := float64(-numberOfFloatsToRandomize)/100.0
-	endX := float64(numberOfFloatsToRandomize)/100.0
+func calculateNormalDistributionStep(numberOfFloatsToRandomize int, numberOfYears int) (float64,float64,float64){
+	startX := float64(-numberOfFloatsToRandomize*numberOfYears)/(100.0)
+	endX := float64(numberOfFloatsToRandomize*numberOfYears)/(100.0)
 	stepSize :=  (math.Abs(startX)+endX) / float64(numberOfFloatsToRandomize)
 
 	//fmt.Println(startX,endX,stepSize)
 	return startX, endX, stepSize
 }
+*/
 
 // Create a daily Energy Price based on the demand(consumed energy) and supply(produced energy)
-func calculateDailyEnergyPrice(dailyEnergyConsumption float64, dailyEnergyProduced float64) (float64, bool){
+func calculateDailyEnergyPrice(producedEnergy float64,  consumedEnergy float64) (float64, bool){
 	// High Consumption & Low Producing -> Low Price
 	// Low Consumption & High Producing -> High Price
 
 	basePrice := 10.0
-	demandSupplyFactor :=  dailyEnergyConsumption / dailyEnergyProduced
+	demandSupplyFactor :=  consumedEnergy / producedEnergy
 
-	isSurplusStatus := isSurplus(dailyEnergyConsumption, dailyEnergyProduced)
+	isSurplusStatus := isSurplus(consumedEnergy, producedEnergy)
 	dailyElectrictyPrice := basePrice * demandSupplyFactor
 
 	return dailyElectrictyPrice, isSurplusStatus
@@ -131,4 +270,10 @@ func calculateDailyEnergyPrice(dailyEnergyConsumption float64, dailyEnergyProduc
 // If over or under baseprice.
 func isSurplus(dailyEnergyConsumption float64, dailyEnergyProduced float64) bool{
 	return dailyEnergyProduced > dailyEnergyConsumption
+}
+
+// Returns the net energy production.
+func calculateNetEnergyProduction(producedEnergy float64,  consumedEnergy float64) float64{
+	calculateNetEnergyProduction := producedEnergy - consumedEnergy
+	return calculateNetEnergyProduction
 }
--- model.go
+++ model.go
@@ -6,30 +6,129 @@ import (
 	"time"
 )
 
+// For one household on one day.
 type EnergyDataPoint struct {
 	Date            string  `json:"date"`
 	ProducedEnergy float64 `json:"producedenergy"`
 	ConsumedEnergy float64 `json:"consumedenergy"`
 	EnergyPrice    float64 `json:"energyprice"`
 	EnergySurplus     bool `json:"energysurplus"`
+	NetEnergyProduction float64 `json:"netenergyproduction"`
+}
+
+type Household struct {
+	ID int `json:"id"`
+	EnergyBuffer  float64 `json:"energybuffer"` // TODO
+	EnergyBuyingRatio float64 `json:"energybuyingratio"` // TODO
+	EnergySellingRatio float64 `json:"energysellingratio"` // TODO
+	DataPoints []EnergyDataPoint `json:"datapoints"`
 }
 
 // Used for getterrs
 var datapoints []EnergyDataPoint
+var households []Household
+
 
 func initializeModel(){
-	rand.Seed(time.Now().UnixNano())
 
-	datapoints = createDataPoints(1)
+	rand.Seed(time.Now().UnixNano())
+	households = createHouseHolds(1,3)
+
+	// TODO  Fixa så att det är lite varians i datan, så inte alla får samma sinus kurva.
+}
+func playModel(){
+	// TODO Load first date and load each households data for that date.
+	// TODO Every 5seconds move to to next date
+	// TODO for each date, add datapoint.NetEnergyProduction to Household.Buffer
+
 }
 
-func isBroken() bool{
-	// 0.00001% chance that windmill will be broken.
-	// If so, cancel that day's output and the following n days.
-	// maybe done at end when in dictionary for easy changing.
-	var status = false
-	return status
+// Create Household (User) with its own datapoint list.
+func createHouseHolds(howManyHouseholds int, datapointsForHowManyYears int) []Household{
+	numberOfYears := datapointsForHowManyYears
+	numberOfHouseholds := howManyHouseholds
 
+	newHouseholdList := make([]Household, numberOfHouseholds )
+
+	for i := 0; i < numberOfHouseholds; i++{
+		household := Household{}
+		household.ID = i
+		household.EnergyBuffer = 0
+		household.EnergyBuyingRatio = 0.5
+		household.EnergySellingRatio = 0.5
+		household.DataPoints = createDataPoints(numberOfYears)
+
+		newHouseholdList[i] = household
+	}
+	return newHouseholdList
+}
+
+// Generate Date Range and Energy Values and put them into a list of structs
+func createDataPoints(datapointsForHowManyYears int) []EnergyDataPoint {
+	numberOfYears := datapointsForHowManyYears
+	numberOfDates := getNumberOfDays(numberOfYears)
+	fmt.Println("dates :", numberOfDates)
+
+	// Generate Data
+	// Wind is gradual and season like -> Sine Wave.  HouseholdPower is consistent (seasonal variation might occur though) -> Randomfloats in range.
+	generatedEnergyProduction := generateSineWaveFloats(10.0, 20.0, numberOfDates)
+	generatedEnergyConsumption := randomizeSeasonalFloats(numberOfDates,numberOfYears)// generateNormalDistibutionFloats(numberOfDates, numberOfYears)
+	//generatedDailyEnergyPrice, generatedDailySurplusStatus := generateEnergyPrice(generatedEnergyConsumption, generatedEnergyProduction)
+	generatedDateRange := createDateRange(2021,01,1,numberOfYears)
+
+	fmt.Println("Length of generatedEnergyProduction", len(generatedEnergyProduction))
+	fmt.Println("Length of generatedEnergyConsumption", len(generatedEnergyConsumption))
+	//fmt.Println("Length of generatedDailyEnergyPrice", len(generatedDailyEnergyPrice))
+	fmt.Println("Length of generatedDateRange", len(generatedDateRange))
+	//fmt.Println("Length of generatedDailySurplusStatus", len(generatedDailySurplusStatus))
+
+	newDatapointsList := make([]EnergyDataPoint,len (generatedDateRange) )
+	daysOfOutage := 0
+
+	// Add Date and ProducedEnergy into structs and add them to the struct list.
+	for i := 0; i < numberOfDates; i++{
+		datapoint := EnergyDataPoint{}
+		datapoint.Date = generatedDateRange[i]
+
+		// If Windmill breaks, an outage will occur for 2-5 days.
+		if daysOfOutage == 0 && isWindmillBroken(){
+			daysOfOutage = generateRandomInteger(3,6)
+		}
+
+		// If Windmill broken, produced energy will plummet (Still a value so no infinite electricity price)
+		// Otherwise take from datalist
+		if daysOfOutage !=0{
+			datapoint.ProducedEnergy = 1.000000000000001
+			daysOfOutage--
+		}else{
+			datapoint.ProducedEnergy = generatedEnergyProduction[i]
+		}
+		datapoint.ConsumedEnergy = generatedEnergyConsumption[i]
+		datapoint.EnergyPrice, datapoint.EnergySurplus = calculateDailyEnergyPrice(datapoint.ProducedEnergy, datapoint.ConsumedEnergy)
+		datapoint.NetEnergyProduction = calculateNetEnergyProduction(datapoint.ProducedEnergy, datapoint.ConsumedEnergy)
+
+	/*	fmt.Println("Date: ", datapoint.Date, " ProducedEnergy:  ",datapoint.ProducedEnergy,
+			" ConsumedEnergy:  ",datapoint.ConsumedEnergy , " EnergyPrice:  ",datapoint.EnergyPrice ,
+		)
+	*/
+
+		//	" EnergySurplus:  ",datapoint.EnergySurplus, " NetEnergyProduction:  ",datapoint.NetEnergyProduction)
+
+		fmt.Println(datapoint.Date,"Price:",calculateConsumerPrice( datapoint.ProducedEnergy ,  datapoint.ConsumedEnergy ,datapoint.EnergyPrice, datapoint.EnergySurplus))
+		newDatapointsList[i] = datapoint
+	}
+	return newDatapointsList
+}
+func updateListAfterDowntime(datapoint EnergyDataPoint){
+	datapoint.ProducedEnergy = 0
+	datapoint.EnergySurplus = isSurplus(datapoint.ConsumedEnergy , datapoint.ProducedEnergy)
+	datapoint.NetEnergyProduction = calculateNetEnergyProduction(datapoint.ProducedEnergy, datapoint.ConsumedEnergy)
+}
+
+
+func getNumberOfDays(numberOfYears int) int{
+	numberOfDates := (numberOfYears * 365) +1
+	return numberOfDates
 }
 
 // Calculate the Final Consumer Price Per Day.
@@ -46,47 +145,18 @@ func calculateConsumerPrice(producedEnergy float64,  consumedEnergy float64,ener
 		return consumerPrice
 	}
 }
+// Checks if windmill is to suffer an outage.
+func isWindmillBroken() bool{
+	randomInteger := generateRandomInteger(1,500)
 
-
-// Generate Date Range and Wind Energy Values and put them into a list of structs
-func createDataPoints(datapointsForHowManyYears int) []EnergyDataPoint {
-	numberOfYears := datapointsForHowManyYears
-	numberOfDates := getNumberOfDays(numberOfYears)
-
-	// Generate Data
-	generatedProducedWindEnergy := generateSineWaveFloats(10.0, 20.0, numberOfDates)
-	generatedHouseholdEnergyConsumption := generateNormalDistibutionFloats(30.0, numberOfDates)
-	generatedDailyEnergyPrice, generatedDailySurplusStatus := generateEnergyPrice(generatedHouseholdEnergyConsumption,generatedProducedWindEnergy)
-	generatedDateRange := createDateRange(2021,01,1,numberOfYears)
-
-	fmt.Println("Length of generatedProducedWindEnergy", len(generatedProducedWindEnergy))
-	fmt.Println("Length of generatedHouseholdEnergyConsumption", len(generatedHouseholdEnergyConsumption))
-	fmt.Println("Length of generatedDailyEnergyPrice", len(generatedDailyEnergyPrice))
-	fmt.Println("Length of generatedDateRange", len(generatedDateRange))
-	fmt.Println("Length of generatedDailySurplusStatus", len(generatedDailySurplusStatus))
-
-	newDatapointsList := make([]EnergyDataPoint,len (generatedDateRange) )
-
-	// Add Date and ProducedEnergy into structs and add them to the struct list.
-	for i := 0; i < numberOfDates; i++{
-		datapoint := EnergyDataPoint{}
-		datapoint.Date = generatedDateRange[i]
-		datapoint.ProducedEnergy = generatedProducedWindEnergy[i]
-		datapoint.ConsumedEnergy = generatedHouseholdEnergyConsumption[i]
-		datapoint.EnergyPrice = generatedDailyEnergyPrice[i]
-		datapoint.EnergySurplus = generatedDailySurplusStatus[i]
-
-		fmt.Println("Date: ", datapoint.Date, " ProducedEnergy:  ",datapoint.ProducedEnergy, " ConsumedEnergy:  ",datapoint.ConsumedEnergy , " EnergyPrice:  ",datapoint.EnergyPrice , " EnergySurplus:  ",datapoint.EnergySurplus)
-		fmt.Println(datapoint.Date," PRICE: ", calculateConsumerPrice(datapoint.ProducedEnergy,datapoint.ConsumedEnergy,datapoint.EnergyPrice,datapoint.EnergySurplus))
-		newDatapointsList[i] = datapoint
+	// 1/500 Chance for Windmill to break.
+	if randomInteger == 42{
+		return true
+	}else{
+		return false
 	}
-	return newDatapointsList
 }
 
-func getNumberOfDays(numberOfYears int) int{
-	numberOfDates := (numberOfYears * 365) +1
-	return numberOfDates
-}
 
 // REST API
 func getWindSpeed(date string) float64{
