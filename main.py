d75d47c6d1eae9c6f472987a1d261f5525c9e349
robdan7 <robdan-7@student.ltu.se>
2021-11-23 09:57:18 +0100

    you can't just copy paste from another branch

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import datetime
-
+import time
 import model
 
 def main():
     startDate = "2021-01-01"
     formattedStartDate = datetime.datetime.strptime(startDate, '%Y-%m-%d').date()
     households = model.initializeModel(10, formattedStartDate)
+    while True:
+        time.sleep(5)
+        print("alive")
     return
 
 main()
